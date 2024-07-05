225c53068ef3a5108b6bc198934b9898cad77e0f
Faisal Aprianto <fa.aprianto.27@gmail.com>
2024-07-06 03:12:23 +0700

    fix swagger

--- routes/api.php
+++ routes/api.php
@@ -65,7 +65,6 @@ Route::middleware([
         //Kas
         Route::get("/kas/month/xls",[KasController::class,"exportKas"]);
         Route::get("/kas/month",[KasController::class,"getMonthKas"]);
-        Route::get("/kas/user/{user_id}",[KasController::class,'getByUser']);
         Route::get("/kas/activity/{activity_id}",[KasController::class,"getByActivityId"]);
         Route::get("/kas",[KasController::class,'index']);
         Route::post("/kas",[KasController::class,'store']);
